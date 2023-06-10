41e84d95997b6658961a1a0809184446f051f028
Barry1003 <isholausamah@gmail.com>
2023-06-10 18:22:07 +0100

    updsate

--- README.md
+++
@@ -1,2 +0,0 @@
-# young_dev-projects
-buliding various project and task to sharpen our tech skills
---
+++ potfolio2/script.js
@@ -0,0 +1,4 @@
+const styleSwitcherToggle = document.querySlector(".style-switcher-toggle");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.queryElector(".style-switcher").classlist.toggle("open");
+})
